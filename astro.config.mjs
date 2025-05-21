1c4ad72484f8f6432ab22d269eb1905090ef0f89
Dhiraj Arun <dhrjarun@gmail.com>
2025-05-21 14:47:05 +0530

    New version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,4 @@
 import { defineConfig, envField } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
 import remarkParse from 'remark-parse';
 import rehypeStringify from 'rehype-stringify';
 import { remarkReadingTime } from './remark-reading-time.mjs';
@@ -8,7 +6,6 @@ import vercel from '@astrojs/vercel';
 import sitemap from '@astrojs/sitemap';
 import remarkToc from 'remark-toc';
 
-import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,7 +17,7 @@ export default defineConfig({
       })
     }
   },
-  integrations: [tailwind(), mdx(), react(), sitemap()],
+  integrations: [sitemap()],
   site: 'https://dhrjarun.com',
   base: '/',
   markdown: {
--- src/content.config.ts
+++ src/content.config.ts
@@ -3,58 +3,8 @@ import { defineCollection, z } from "astro:content";
 import { CONTENT_PATH } from "astro:env/server";
 import { join } from "node:path";
 
-const writing = defineCollection({
-	loader: glob({ pattern: "*.md", base: join(CONTENT_PATH, "/Writing") }),
-	schema: z
-		.object({
-			title: z.string(),
-			publishDate: z.date(),
-			draft: z.boolean().optional().default(false),
-			archive: z.boolean().optional().default(false),
-		})
-		.passthrough(),
+const posts = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: CONTENT_PATH, }),
 });
 
-const projects = defineCollection({
-	loader: glob({
-		pattern: "*.md",
-		base: join(CONTENT_PATH, "/projects"),
-	}),
-	schema: z
-		.object({
-			title: z.string(),
-			lastUpdated: z.date().optional(),
-			start: z.number(),
-			end: z.number().optional().nullable(),
-			descriptions: z.string(),
-		})
-		.passthrough(),
-});
-
-const about = defineCollection({
-	loader: glob({ pattern: "*.md", base: join(CONTENT_PATH, "/About") }),
-	schema: z
-		.object({
-			publishDate: z.date(),
-			lastUpdated: z.date().optional(),
-			draft: z.boolean().optional(),
-		})
-		.passthrough(),
-});
-
-const now = defineCollection({
-	loader: glob({ pattern: "*.md", base: join(CONTENT_PATH, "/Now") }),
-	schema: z
-		.object({
-			publishDate: z.date().optional(),
-			lastUpdated: z.date().optional(),
-			draft: z.boolean().optional(),
-		})
-		.passthrough(),
-});
-
-const root = defineCollection({
-	loader: glob({ pattern: "*.md", base: join(CONTENT_PATH, "/") }),
-});
-
-export const collections = { writing, projects, about, now, root };
+export const collections = { posts };
